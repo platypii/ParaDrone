d03b6d8e5d06a7ada9dbca5eac1bad8e8d5280ad
Kenny Daniel <platypii@gmail.com>
2020-04-14 23:17:47 -0700

    Map landing

--- android/app/src/main/java/ws/baseline/autopilot/geo/SegmentTurn.java
+++ android/app/src/main/java/ws/baseline/autopilot/geo/SegmentTurn.java
@@ -1,9 +1,12 @@
 package ws.baseline.autopilot.geo;
 
+import android.util.Log;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SegmentTurn implements Segment {
+    private static final String TAG = "SegmentTurn";
+
     private final Circle circle;
     private final Point start;
     private final Point end;
@@ -28,6 +31,9 @@ public class SegmentTurn implements Segment {
 
     @Override
     public PathLike fly(double distance) {
+        if (distance < 0) {
+            Log.e(TAG, "Flight distance cannot be negative " + distance);
+        }
         final double len = length();
         if (distance < len) {
             final double theta = angle1() + turn * distance / circle.radius;
@@ -41,8 +47,8 @@ public class SegmentTurn implements Segment {
             final double dx = end.x - circle.x;
             final double dy = end.y - circle.y;
             final Point proj = new Point(
-                    end.x + remaining * dy / circle.radius,
-                    end.y - remaining * dx / circle.radius
+                    end.x + turn * remaining * dy / circle.radius,
+                    end.y - turn * remaining * dx / circle.radius
             );
             final SegmentLine line = new SegmentLine(end, proj);
             return new Path(this, line);
@@ -51,19 +57,15 @@ public class SegmentTurn implements Segment {
 
     @Override
     public double length() {
-        double arcs = turn * (angle2() - angle1());
-        if (arcs < 0) arcs += 2 * Math.PI;
-        return circle.radius * arcs;
+        return circle.radius * arcs();
     }
 
     @Override
     public List<Point> render() {
         final List<Point> points = new ArrayList<>();
         final double angle1 = angle1();
-        final double angle2 = angle2();
-        final double step = 0.1; // ~6 degrees
-        double arcs = turn * (angle2 - angle1);
-        if (arcs < 0) arcs += 2 * Math.PI;
+        final double step = 0.05; // ~3 degrees
+        final double arcs = arcs();
         for (double delta = 0; delta < arcs; delta += step) {
             final double theta = angle1 + turn * delta;
             points.add(new Point(
@@ -73,6 +75,15 @@ public class SegmentTurn implements Segment {
         }
         points.add(end);
         return points;
+    }
+
+    /**
+     * The arc angle in radians
+     */
+    private double arcs() {
+        double arcs = this.turn * (this.angle2() - this.angle1());
+        if (arcs < 0) arcs += 2 * Math.PI;
+        return arcs;
     }
 
     /**
