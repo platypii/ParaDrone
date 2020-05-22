bad05f773bce283115bccf96ce0efc931ae0e8fb
Kenny Daniel <platypii@gmail.com>
2020-05-21 21:42:58 -0700

    Move ApScreen to its own fragment

--- android/app/src/main/java/ws/baseline/autopilot/bluetooth/APLocation.java
+++ android/app/src/main/java/ws/baseline/autopilot/bluetooth/APLocation.java
@@ -2,6 +2,9 @@ package ws.baseline.autopilot.bluetooth;
 
 import ws.baseline.autopilot.GeoPoint;
 
+/**
+ * Fused location and speed
+ */
 public class APLocation implements APEvent {
     public APLocation(GeoPoint lastPoint) {
     }
