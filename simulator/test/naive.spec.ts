9c383810f40eb2a339411e57e5b736d3617ae49a
Kenny Daniel <platypii@gmail.com>
2020-10-14 23:41:40 -0700

    Testy

---
+++ simulator/test/naive.spec.ts
@@ -0,0 +1,15 @@
+import { naive } from "../ts/plan/planner-naive"
+import { assert } from 'chai'
+
+const r = 100
+
+describe('Naive planner', () => {
+  it('should find a plan', () => {
+    const start = {x: 1000, y: 1000, vx: 10, vy: 0}
+    const end = {x: 0, y: 0, vx: 10, vy: 0}
+    const path = naive(start, end, r)
+    assert.equal(path!.name, "naive")
+    assert.equal(path!.segments.length, 2)
+    assert.approximately(path!.length(), 1590, 0.1)
+  })
+})
